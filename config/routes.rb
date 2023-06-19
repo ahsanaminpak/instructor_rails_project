a78e57d5fddc03d3b0fb2dd3d2dce40613a92207
ahsanaminpak <ahsan.amin@pakeventures.com>
2023-06-19 07:18:51 +0500

    completed the devise functionality and added the Users model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+
+  # devise_for :users
+  
+  # get 'review/index'
+  root "review#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
