7c07869d58432d7d1dd3e4e48d52d8ee70a2da3f
ahsanaminpak <ahsan.amin@pakeventures.com>
2023-06-21 18:48:05 +0500

    fixed searchable after_commit

--- app/models/concerns/searchable.rb
+++ app/models/concerns/searchable.rb
@@ -8,7 +8,8 @@ module Searchable
 
     after_commit :index_document, if: :persisted?
     after_commit on: [:destroy] do
-      __elasticsearch__.delete_document
+      # __elasticsearch__.delete_document
+      __elasticsearch__.refresh_index!
     end
   end
 
